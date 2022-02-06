679b8e63aeef7f3611c041b30f86fa79b08c1881
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-02-06 12:18:03 -0800

    Use remote data files hosted on Universalis

--- UniversalisCommon/RemoteDataLocations.cs
+++ UniversalisCommon/RemoteDataLocations.cs
@@ -2,7 +2,7 @@
 {
     internal class RemoteDataLocations
     {
-        public const string OpcodeDefinitions = "https://raw.githubusercontent.com/goaaats/universalis_act_plugin/master/definitions.json";
-        public const string Version = "https://raw.githubusercontent.com/goaaats/universalis_act_plugin/master/version";
+        public const string OpcodeDefinitions = "https://act.universalis.app/definitions.json";
+        public const string Version = "https://act.universalis.app/version";
     }
 }
